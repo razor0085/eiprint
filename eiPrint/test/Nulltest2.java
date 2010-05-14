b9c0d55d0a8f082bf500f6ad4c97388ec953ca43
gion.padrun@gmail.com <gion.padrun@gmail.com@47bc85c6-d93c-11de-849f-b362a63f070a>
2010-05-14 07:24:20 +0000

    Test serial to MC

---
+++ eiPrint/test/Nulltest2.java
@@ -0,0 +1,185 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package test;
+/**
+ *
+ * @author padrun
+ */
+// derived from SUN's examples in the javax.comm package
+import java.io.*;
+import java.util.*;
+//import javax.comm.*; // for SUN's serial/parallel port libraries
+import gnu.io.*; // for rxtxSerial library
+
+public class Nulltest2 implements Runnable, SerialPortEventListener {
+   static CommPortIdentifier portId;
+   static CommPortIdentifier saveportId;
+   static Enumeration        portList;
+   InputStream           inputStream;
+   SerialPort           serialPort;
+   Thread           readThread;
+
+   //static String        messageString = "that's a,test!";
+   static byte              testByes;
+   static OutputStream      outputStream;
+   static boolean        outputBufferEmptyFlag = false;
+
+   public static void main(String[] args) {
+      boolean           portFound = false;
+      String           defaultPort;
+
+      // determine the name of the serial port on several operating systems
+      String osname = System.getProperty("os.name","").toLowerCase();
+      if ( osname.startsWith("windows") ) {
+         // windows
+         defaultPort = "COM1";
+      } else if (osname.startsWith("linux")) {
+         // linux
+        defaultPort = "/dev/ttyS0";
+      } else if ( osname.startsWith("mac") ) {
+         // mac
+         defaultPort = "????";
+      } else {
+         System.out.println("Sorry, your operating system is not supported");
+         return;
+      }
+
+      if (args.length > 0) {
+         defaultPort = args[0];
+      }
+
+      System.out.println("Set default port to "+defaultPort);
+
+		// parse ports and if the default port is found, initialized the reader
+      portList = CommPortIdentifier.getPortIdentifiers();
+      while (portList.hasMoreElements()) {
+         portId = (CommPortIdentifier) portList.nextElement();
+         if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+            if (portId.getName().equals(defaultPort)) {
+               System.out.println("Found port: "+defaultPort);
+               portFound = true;
+               // init reader thread
+               Nulltest2 reader = new Nulltest2();
+            }
+         }
+
+      }
+      if (!portFound) {
+         System.out.println("port " + defaultPort + " not found.");
+      }
+
+   }
+
+   public void initwritetoport() {
+      // initwritetoport() assumes that the port has already been opened and
+      //    initialized by "public nulltest()"
+
+      try {
+         // get the outputstream
+         outputStream = serialPort.getOutputStream();
+      } catch (IOException e) {}
+
+      try {
+         // activate the OUTPUT_BUFFER_EMPTY notifier
+         serialPort.notifyOnOutputEmpty(true);
+      } catch (Exception e) {
+         System.out.println("Error setting event notification");
+         System.out.println(e.toString());
+         System.exit(-1);
+      }
+
+   }
+
+   public void writetoport() {
+      byte[] testBytes = new byte[6];
+      testBytes[0] = 100;
+      System.out.println("Writing " + testBytes + "to " + serialPort.getName());
+      
+
+      try {
+         // write string to serial port
+         outputStream.write(testBytes);
+      } catch (IOException e) {}
+   }
+
+   public Nulltest2() {
+      // initalize serial port
+      try {
+         serialPort = (SerialPort) portId.open("SimpleReadApp", 2000);
+      } catch (PortInUseException e) {}
+
+      try {
+         inputStream = serialPort.getInputStream();
+      } catch (IOException e) {}
+
+      try {
+         serialPort.addEventListener(this);
+      } catch (TooManyListenersException e) {}
+
+      // activate the DATA_AVAILABLE notifier
+      serialPort.notifyOnDataAvailable(true);
+
+      try {
+         // set port parameters
+
+          //serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8,
+          serialPort.setSerialPortParams(62500, SerialPort.DATABITS_8,
+          SerialPort.STOPBITS_1,
+                     SerialPort.PARITY_NONE);
+      } catch (UnsupportedCommOperationException e) {}
+
+      // start the read thread
+      readThread = new Thread(this);
+      readThread.start();
+
+   }
+
+   public void run() {
+      // first thing in the thread, we initialize the write operation
+      initwritetoport();
+      try {
+         while (true) {
+            // write string to port, the serialEvent will read it
+            writetoport();
+            Thread.sleep(1000);
+         }
+      } catch (InterruptedException e) {}
+   }
+
+   public void serialEvent(SerialPortEvent event) {
+      switch (event.getEventType()) {
+      case SerialPortEvent.BI:
+      case SerialPortEvent.OE:
+      case SerialPortEvent.FE:
+      case SerialPortEvent.PE:
+      case SerialPortEvent.CD:
+      case SerialPortEvent.CTS:
+      case SerialPortEvent.DSR:
+      case SerialPortEvent.RI:
+      case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
+         break;
+      case SerialPortEvent.DATA_AVAILABLE:
+         // we get here if data has been received
+         byte[] readBuffer = new byte[20];
+         try {
+            // read data
+            while (inputStream.available() > 0) {
+               int numBytes = inputStream.read(readBuffer);
+            }
+            // print data
+            String result  = new String(readBuffer);
+
+
+            System.out.println("Read: "+result);
+         } catch (IOException e) {}
+
+         break;
+      }
+   }
+
+}
+
