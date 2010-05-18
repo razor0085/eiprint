dd8303a050d307937caef43d1ea41e593a8ed3b6
dmalgiaritta@gmail.com <dmalgiaritta@gmail.com@47bc85c6-d93c-11de-849f-b362a63f070a>
2010-05-18 10:34:39 +0000

    db added tamalgia

--- src/eiPrint/OBJMgmt/Koordinatentransformation.java
+++ src/eiPrint/OBJMgmt/Koordinatentransformation.java
@@ -24,8 +24,14 @@ public class Koordinatentransformation
     private double tz = 550.0D;
     //Ansicht von Vorne Laser auf meiner Seite, 70/2
     private double ty = 35.0D; //entspricht ty
-    
-    public Koordinatentransformation(int x, int y, int z)
+
+
+    public Koordinatentransformation()
+    {
+
+    }
+
+    public Koordinatentransformation(double x, double y, double z)
     {
         this.x=x;
         this.y=y;
@@ -42,121 +48,108 @@ public class Koordinatentransformation
      */
     public void koordinatensystemDrehung()
     {
+
         //Zuerst wird das Koordinatensystem um 45° gedreht
         xrobot = x * Math.cos(winkel) + z * Math.sin(winkel);
         zrobot = (-1) * x *Math.sin(winkel) + z * Math.cos(winkel);
         //Dann wird es um Vektor t(0,ty,tz) veschoben, x bleibt unberändert
-        yrobot = y + ty;
+        // in y wird nicht mehr verschoben, durch neuem Release muss Nullpunkt
+        //nur in Z-Richtung verschoben werden
+        //yrobot = y + ty;
+
         zrobot = z + tz;
 
     }
-
- 
     /**
      * @return the x
      */
     public double getX() {
         return x;
     }
-
     /**
      * @param x the x to set
      */
     public void setX(double x) {
         this.x = x;
     }
-
     /**
      * @return the y
      */
     public double getY() {
         return y;
     }
-
     /**
      * @param y the y to set
      */
     public void setY(double y) {
         this.y = y;
     }
-
     /**
      * @return the z
      */
     public double getZ() {
         return z;
     }
-
     /**
      * @param z the z to set
      */
     public void setZ(double z) {
         this.z = z;
     }
-
     /**
      * @return the xrobot
      */
     public double getXrobot() {
         return xrobot;
     }
-
     /**
      * @param xrobot the xrobot to set
      */
     public void setXrobot(double xrobot) {
         this.xrobot = xrobot;
     }
-
     /**
      * @return the yrobot
      */
     public double getYrobot() {
         return yrobot;
     }
-
     /**
      * @param yrobot the yrobot to set
      */
     public void setYrobot(double yrobot) {
         this.yrobot = yrobot;
     }
-
     /**
      * @return the zrobot
      */
     public double getZrobot() {
         return zrobot;
     }
-
     /**
      * @param zrobot the zrobot to set
      */
     public void setZrobot(double zrobot) {
         this.zrobot = zrobot;
     }
-
     /**
      * @return the winkel
      */
     public double getWinkel() {
         return winkel;
     }
-
     /**
      * @param winkel the winkel to set
      */
     public void setWinkel(double winkel) {
         this.winkel = winkel;
     }
-
     /**
      * @return the höhe
      */
     public double getHöhe() {
         return tz;
     }
-
     /**
      * @param höhe the höhe to set
      */
--- src/eiPrint/OBJMgmt/Objektfile_einlesen.java
+++ src/eiPrint/OBJMgmt/Objektfile_einlesen.java
@@ -2,101 +2,204 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package eiPrint.OBJMgmt;
+
+import com.db4o.Db4o;
+import java.io.File;
+import com.db4o.*;
+//import com.db4o.f1.*;
+import eiPrint.OBJMgmt.Util;
+import com.db4o.query.*;
+import com.db4o.ObjectSet;
+import com.db4o.ObjectContainer;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author dave
  */
-public class Objektfile_einlesen {
+public class Objektfile_einlesen extends Util {
     
     //Attribute
+    private ObjectContainer db;
+    //private Koordinatentransformation kt;
     private String pfad;
     private String filename;
     private FileReader fileIn;
-    private ArrayList<String> list;
-  
+    private ArrayList<Point> ptmenge; //Hällt alle Punkte des Druck Objekts
+    private double x,y,z=0;
+    private Point point;
+    private double [] pt;
+
 /**
  * Konstruktor
+ *
  * @param filename
  * @param pfad
  */
+    @SuppressWarnings("empty-statement")
     public Objektfile_einlesen(String pfad)
     {
-        list = new ArrayList<String>();
+
+        //db = Db4o.openFile("C:/Users/david/Desktop/PREN/DB.yap");
+
+        point = new Point();
+        ptmenge = new ArrayList<Point>();
         this.pfad = pfad;
         System.out.println(""+pfad);
         try {
             fileIn = new FileReader(pfad);
+
+
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Objektfile_einlesen.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
 
+
+
+    }
     /**
      * Das .obj File wird eingelesen und in einen Arraylist abgespeichert.
      * @throws ClassNotFoundException
      */
+    @SuppressWarnings("empty-statement")
     public void fileEinlesen() throws ClassNotFoundException, IOException
     {
-        String suchStr = "v";
-//		//braucht es try catch wirklich?
-//        try {
-//             fileIn = new FileReader(pfad);
-//        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(Objektfile_einlesen.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        
         BufferedReader buff = new BufferedReader(fileIn);
-
         //Speichert den BufferInhalt temporär
-        String tmp = "";
-
+        String zeile = "";
         //Lies bis nix mehr da ist (Rückgabewert = null statt String)
-        while ((tmp = buff.readLine()) != null)
+        while ((zeile = buff.readLine()) != null)
         {
-        //Mach irgendwas mit der eingelesenen Zeile (speichern, splitten o.ä.)
-            list.add(tmp);
-        }
 
+            Scanner scanner = new Scanner(zeile);
+            if(zeile.startsWith("v ")  ){
+            //x ,y,z Koordinate isolieren, zaehler richtig nullen!!!       
+            int zaehler = 0;
+            while ((scanner.hasNext())) {
+                if (scanner.hasNextDouble()) {
+                    if (zaehler == 0) {
+                        x = scanner.nextDouble();
+                        //System.out.println(x);
+                        zaehler++;
+                    } else if (zaehler == 1) {
+                        y = scanner.nextDouble();
+                        zaehler++;
+                       // System.out.println(y);
+                    } else if (zaehler == 2) {
+                        z = scanner.nextDouble();
+                        zaehler=0;
+                        //System.out.println(z);
+                    }
+
+                } else {
+                    scanner.next();
+                   }
+            }
+
+         //x,y,z transformieren
+         Koordinatentransformation kt = new Koordinatentransformation(x,y,z);
+         kt.koordinatensystemDrehung();
+        //koord in point schreiben
+//        point.setX(x);
+//        point.setY(y);
+//        point.setZ(z);
+            point.setX(kt.getX());
+            point.setY(kt.getY());
+            point.setZ(kt.getZ());
+
+            ptmenge.add(new Point(x,y,z));
+           //System.out.println("ptmenge"+point);
+            
+            }
+            else{
+                
+                zeile+=buff.readLine();
+                System.out.println("Kein v!!");
+            }
+      
+        }
+       
         //InputStream schliessen
-        buff.close();
-        fileIn.close();
+          buff.close();
+          fileIn.close();
+          saveToDb();
     }
 
-    /**
-     * Suche nach der nächsten Koordinate ev. in fileEinlesen drin
-     */
-    public void leseKoordinate()
+    public void saveToDb()
     {
-        int count = 0;
-        String suchStr = "v";
-        while (count < 50) {
-            for (int i = 0; i < list.size(); i++)
-            {
-                count++;
+        ObjectContainer db = Db4o.openFile("C:/Users/david/Desktop/PREN/DB.yap");
+        try {
+            // do something with db4o
+           for(int i =0; i<ptmenge.size();i++){
+                db.store(new Point(ptmenge.get(i).getX(),ptmenge.get(i).getY()
+                        ,ptmenge.get(i).getZ()));  // Hier wird das Objekt gespeichert 
+            System.out.println("Stored " + (ptmenge.get(i).getX()+" "+ptmenge.get(i).getY() +" " + ptmenge.get(i).getZ()));
+           }
+           db.commit();
+        } finally {
+            db.close();
+        }
+    }
 
-                if(list.get(i).equalsIgnoreCase(suchStr))
-                {
-                    System.out.println(list.get(i));
-                }
-                
+//    public Point getZballon()
+//    {
+////        if(ptmenge.get(i).getX()){
+////        return z;
+////        }
+//        List<Point> points = db.query(new Predicate<Point>() {
+//
+//            public boolean match(Point point) {
+//                return ((point.getX() == 100)&& point.getY()==100);
+//            }
+//        });
+//    }
+    /**
+     * Sucht in der Db nach einem Punkt welcher die x,y-Koordinate möglichst
+     * nahe bei null ist.
+     * @return
+     */
+    public double getOrigin()
+    {
+//        for(int i = 0; i<ptmenge.size();i++){
+//       if(ptmenge.get(i).getX()==0 && ptmenge.get(i)getY()==0){
+//
+//        return 0.0;
+//       }
+//        }
+        return 0;
+    }
+    /**
+     * Druckt alle punkte aus
+     *
+     */
+    public static void retrievePoints(ObjectContainer db) {
+        ObjectSet result = db.get(Point.class);
+        listResult(result);
+    }
 
+    public double getZkoordinate() {
+        List<Point> result = db.query(new Predicate<Point>() {
+
+            public boolean match(Point point) {
+                return point.getPoint() > 99 && point.getPoint() < 199;
 
             }
-        }
-    }    
+        });
+        return 0.0D;
+    }
     /**
      * @return the filename
      */
     public String getFilename() {
         return filename;
     }
-
     /**
      * @param filename the filename to set
      */
@@ -104,19 +207,43 @@ public class Objektfile_einlesen {
         this.filename = filename;
     }
 
-    @SuppressWarnings("static-access")
    public static void main(String[] args)
    {
-       String path = "C:/Dokumente und Einstellungen/MrCrow/Eigene Dateien/My Dropbox/scoula/PREN2/Pojekt/eiPrint/src/eiPrint/OBJMgmt/data/BallonMaximalkorrigiert.txt";
-       System.out.println("Start");
-       Objektfile_einlesen file = new Objektfile_einlesen(path);
-       try
-       {
-        file.fileEinlesen();
+       //C:/Dokumente und Einstellungen/dave/Desktop/PREN/eiPrint/src/eiPrint/OBJMgmt/data
+       Objektfile_einlesen o = new Objektfile_einlesen("C:/Users/david/Desktop/PREN/eiPrint/src/eiPrint/OBJMgmt/data/BallonMaximalkorrigiert.txt");
+       try{
+       o.fileEinlesen();
+       //o.saveToDb();
+       }catch(Exception e){
+           e.printStackTrace();
        }
-       catch(Exception e){
-        System.out.println("something went wrong!");
-        }
-       file.leseKoordinate();
-    }
+
+      
+
+       //############
+//
+//               Scanner scanner = new Scanner("Bla test /n" +
+//                "v 55.474472 120.473312 0/n " +
+//                "v 0.0 1.11 2.22 /n" + "# 2d texture coordinates (x,y): /n" + "vt 0.394984 0.815900 ");
+//
+//         // accessDb4o
+//        ObjectContainer db = Db4o.openFile("C:/Dokumente und Einstellungen/dave/" +
+//                "Desktop/PREN/DB.yap");
+//        try {
+//            // do something with db4o
+//            for(int i = 0; i<ptmenge.size();i++){
+//            db.set(ptmenge.get(i));
+//            System.out.println("Stored " + point);
+//            }
+//        } finally {
+//            db.close();
+//        }
+        // storeFirstPilot
+
+       
+
+
+   }
+
+   
 }
